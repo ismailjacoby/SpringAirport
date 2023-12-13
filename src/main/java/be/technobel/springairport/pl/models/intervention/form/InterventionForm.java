c21ca2e17eb18eacb422dc28098df72b865d19a4
Jacoby Ismail <ismail.jacoby@gmail.com>
2023-12-13 15:45:47 +0100

    Contrainte FutureOrPresent & Puissance/Place > 1

--- src/main/java/be/technobel/springairport/pl/models/intervention/form/InterventionForm.java
+++ src/main/java/be/technobel/springairport/pl/models/intervention/form/InterventionForm.java
@@ -1,10 +1,14 @@
 package be.technobel.springairport.pl.models.intervention.form;
 
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.FutureOrPresent;
+
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 public record InterventionForm(
         String objet,
+        @FutureOrPresent
         LocalDate date,
         LocalTime time) {
 }
--- src/main/java/be/technobel/springairport/pl/models/typeAvion/form/TypeAvionForm.java
+++ src/main/java/be/technobel/springairport/pl/models/typeAvion/form/TypeAvionForm.java
@@ -1,8 +1,12 @@
 package be.technobel.springairport.pl.models.typeAvion.form;
 
+import jakarta.validation.constraints.Min;
+
 public record TypeAvionForm(
         String nom,
         String constucteur,
+        @Min(value = 1)
         int puissance,
+        @Min(value = 1)
         int nbPlaces) {
 }
