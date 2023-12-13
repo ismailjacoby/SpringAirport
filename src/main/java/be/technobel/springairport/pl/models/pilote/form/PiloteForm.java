9e40c66ae556717688ba6e4eb0ec34d9c31e10e2
Jacoby Ismail <ismail.jacoby@gmail.com>
2023-12-13 14:15:03 +0100

    Removed numBrevet from PiloteForm

--- src/main/java/be/technobel/springairport/pl/models/pilote/form/PiloteForm.java
+++ src/main/java/be/technobel/springairport/pl/models/pilote/form/PiloteForm.java
@@ -3,6 +3,5 @@ package be.technobel.springairport.pl.models.pilote.form;
 public record PiloteForm(
         String nom,
         String adresse,
-        String tel,
-        String numBrevet) {
+        String tel) {
 }
